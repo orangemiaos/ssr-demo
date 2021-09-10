9f8d85a21971688735632f95a15c417939692aae
“zm9” <zm9@meitu.com>
2021-09-10 10:11:44 +0800

    配置自动打包编译

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-const Home = () => (
-  <div>
-    <span>hello react!</span>
-    <p>我是meng</p>
-  </div>
-);
+const Home = () => {
+  function handleClick() {
+    alert("执行点击事件");
+  }
+
+  return (
+    <div>
+      <span>hello react!</span>
+      <button onClick={handleClick}>点击</button>
+    </div>
+  );
+};
 
 export default Home;
